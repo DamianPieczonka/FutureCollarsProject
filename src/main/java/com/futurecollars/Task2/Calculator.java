7193c51d322ff3daf60c1368695a92271f3227c5
DamianPieczonka <damian.pieczonka01@gmail.com>
2021-11-07 18:30:58 +0100

    Maven Task 3 - Added changes v4 - deleted main

--- src/main/java/com/futurecollars/Task2/Calculator.java
+++ src/main/java/com/futurecollars/Task2/Calculator.java
@@ -4,13 +4,13 @@ public class Calculator {
 
     public Calculator(){
     }
-    public double sum(double firstArg, double secondArg) {
+    public double sum (double firstArg, double secondArg) {
         return firstArg + secondArg;
     }
-    public double substract(double firstArg, double secondArg) {
+    public double substract (double firstArg, double secondArg) {
         return firstArg - secondArg;
     }
-    public double divide(double firstArg, double secondArg) {
+    public double divide (double firstArg, double secondArg) {
         if(secondArg == 0) {
             throw new RuntimeException();
         }
@@ -18,7 +18,7 @@ public class Calculator {
             return firstArg / secondArg;
         }
     }
-    public double multiply(double firstArg, double secondArg) {
+    public double multiply (double firstArg, double secondArg) {
         return firstArg * secondArg;
     }
 }
