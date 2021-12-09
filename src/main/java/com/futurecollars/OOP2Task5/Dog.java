55685fb3c492136966bcb2a98228a5cbeb3d47e7
DamianPieczonka <damian.pieczonka01@gmail.com>
2021-12-09 20:15:20 +0100

    OOP 2 Task 5

---
+++ src/main/java/com/futurecollars/OOP2Task5/Dog.java
@@ -0,0 +1,14 @@
+package com.futurecollars.OOP2Task5;
+
+public class Dog extends Animal{
+    private int age;
+
+    public Dog(String name, int age) {
+        super(name);
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
