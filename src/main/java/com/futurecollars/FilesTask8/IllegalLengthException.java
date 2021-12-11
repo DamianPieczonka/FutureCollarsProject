4315b3fe59bbc2cbf9323ec93e38f39f583b50d6
DamianPieczonka <damian.pieczonka01@gmail.com>
2021-12-11 17:12:05 +0100

    Files Task 8

---
+++ src/main/java/com/futurecollars/FilesTask8/IllegalLengthException.java
@@ -0,0 +1,7 @@
+package com.futurecollars.FilesTask8;
+
+public class IllegalLengthException extends RuntimeException {
+    public IllegalLengthException(String message) {
+        super(message);
+    }
+}
