41153d9a5bca10a181fa69dc9af162941113dfda
DamianPieczonka <damian.pieczonka01@gmail.com>
2021-12-12 12:22:03 +0100

    Clean Code Task 5 v3

--- src/main/java/com/futurecollars/CleanCodeTask5/VehicleTwo.java
+++ src/main/java/com/futurecollars/CleanCodeTask5/VehicleTwo.java
@@ -1,6 +1,6 @@
 package com.futurecollars.CleanCodeTask5;
 
-public interface Vehicle {
+public interface VehicleTwo {
     public boolean isEngineStarted();
-
+    public boolean isSittingOnBike();
 }
